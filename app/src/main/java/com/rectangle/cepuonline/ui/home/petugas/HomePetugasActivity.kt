4a261bad96d5cd23f660202fb75c14753f4c59ee
Amos Gamaliel <pecbossgams@gmail.com>
2021-04-07 16:27:48 +0700

    title bar dan fitur logut petugas

--- app/src/main/java/com/rectangle/cepuonline/ui/home/petugas/HomePetugasActivity.kt
+++ app/src/main/java/com/rectangle/cepuonline/ui/home/petugas/HomePetugasActivity.kt
@@ -1,11 +1,16 @@
 package com.rectangle.cepuonline.ui.home.petugas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.rectangle.cepuonline.R
+import com.rectangle.cepuonline.data.db.AppDatabase
+import com.rectangle.cepuonline.ui.auth.LoginActivity
+import kotlinx.android.synthetic.main.activity_home_masyarakat.*
 
 class HomePetugasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +21,24 @@ class HomePetugasActivity : AppCompatActivity() {
         findViewById<BottomNavigationView>(R.id.bottom_nav)
             .setupWithNavController(navController)
 
+        val builder = AlertDialog.Builder(this)
 
+        logout.setOnClickListener {
+            builder.setTitle("Keluar?")
+            builder.setMessage("Apakah anda ingin keluar dari akun ini?")
+
+            builder.setPositiveButton(android.R.string.yes) { dialog, which ->
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                AppDatabase.clearTables()
+                finish()
+            }
+
+            builder.setNegativeButton(android.R.string.no) { dialog, which ->
+                dialog.dismiss()
+            }
+            builder.show()
+        }
 
     }
 }
